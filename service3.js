bac092f2eafac1fed407297baafbd12e27abbd80
Mrsmithz <Dechapon.n@hotmail.com>
2022-04-25 17:55:52 +0700

    edit metric name

--- service3.js
+++ service3.js
@@ -16,7 +16,7 @@ const meter = new MeterProvider({
 }).getMeter('demo-prometheus');
 
 // Now, start recording data
-const counter = meter.createCounter('62070070_count_movies', {
+const counter = meter.createCounter('count_movies', {
   description: 'Count movies api'
 });
 
